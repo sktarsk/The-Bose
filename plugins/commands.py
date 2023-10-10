a38bad76c64153732829604be53d8cf86f0d63cf
sktarsk <117266861+sktarsk@users.noreply.github.com>
2023-10-10 21:27:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -255,7 +255,6 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://t.me/TeamLeech')] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
